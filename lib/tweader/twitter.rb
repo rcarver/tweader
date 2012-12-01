fe2006ed1720a9a8d2cbcda677e32680c1dd5479
Ryan Carver <ryan@ryancarver.com>
2012-12-01 13:44:47 -0800

    really send a tweet

--- lib/tweader/twitter.rb
+++ lib/tweader/twitter.rb
@@ -2,13 +2,21 @@ module Tweader
   # The Twitter client which actually posts to Twitter.
   class Twitter
 
+    # Initialize a new Twitter.
+    #
+    # twitter_client - Twitter::Client.
+    #
+    def initialize(twitter_client)
+      @twitter_client = twitter_client
+    end
+
     # Post a new tweet to Twitter.
     #
     # message - String to post.
     #
     # Returns nothing.
     def tweet(message)
-      puts message
+      @twitter_client.update(message)
     end
   end
 end
