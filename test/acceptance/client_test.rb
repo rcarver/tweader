558ebcca22bf852d8337c4fb3f30cab08eb3eb27
Ryan Carver <ryan@ryancarver.com>
2012-12-01 13:45:00 -0800

    configure twitter and protect against bad auth

--- test/acceptance/client_test.rb
+++ test/acceptance/client_test.rb
@@ -4,13 +4,34 @@ describe "acceptance: the client" do
 
   let(:recipient) { "rcarver" }
   let(:label)     { "tweader" }
-  let(:twitter)   { Tweader::Twitter.new }
-  let(:api)       { GoogleReaderApi::Api.new(:email => ENV['GREMAIL'], :password => ENV['GRPASSWORD']) }
-  let(:reader)    { Tweader::Reader.new(api, label) }
-  let(:tweeter)   { Tweader::Tweeter.new(recipient, twitter) }
-  let(:client)    { Tweader::Client.new(reader, tweeter) }
+
+  let(:twitter_client) {
+    Twitter::Client.new(
+      :consumer_key       => ENV['TW_CONSUMER_KEY'],
+      :consumer_secret    => ENV['TW_CONSUMER_SECRET'],
+      :oauth_token        => ENV['TW_OAUTH_TOKEN'],
+      :oauth_token_secret => ENV['TW_OAUTH_SECRET']
+    )
+  }
+  let(:twitter) { Tweader::Twitter.new(twitter_client) }
+  let(:tweeter) { Tweader::Tweeter.new(recipient, twitter) }
+
+  let(:google_reader_api) {
+    GoogleReaderApi::Api.new(
+      :email    => ENV['GR_EMAIL'],
+      :password => ENV['GR_PASSWORD'])
+  }
+  let(:reader) { Tweader::Reader.new(google_reader_api, label) }
+
+  let(:client) { Tweader::Client.new(reader, tweeter) }
 
   it "reads and tweets" do
-    client.tweet(5)
+    begin
+      client.tweet(1)
+    rescue GoogleLogin::ClientLogin::BadAuthentication
+      skip "Google authentication failed"
+    rescue Twitter::Error::BadRequest
+      skip "Twitter authentication failed"
+    end
   end
 end
